ebc4d4ba9f5be1a5456dde9cce0bc29ea5d1c966
Dale Salter <djsalter93@hotmail.com>
2014-02-08 17:40:09 +1100

    Basically functionally working, moves person to end of que in game loop if they haven't made a choice

--- public/javascripts/website.js
+++ public/javascripts/website.js
@@ -1,9 +1,6 @@
 var socket = io.connect();
 
 var givenUserName = '';
-var selected = '';
-
-
 
 $(document).ready(function(){
 
@@ -35,23 +32,30 @@ $(document).ready(function(){
     // Requests a game
     $('#findGameButton').on('click', function(){
         console.log("Find game button clicked!");
-        handleGameAttempt(socket);
-        socket.emit('attemptToFindGame');
-    });
 
+        handleGameAttempt(socket);
+        handleWinLoss(socket);
+        handleFinish(socket);
+
+        socket.emit('attemptToFindGame');
+        $('#findGameButton').text("You've been put in a que!");
+    });
 
 
     // Item clickers
     $('#rock').on('click', function(){
         socket.emit('itemSelected', {item: 'rock'});
+        selection($(this), $('#paper'), $('#scissors'));
     });
 
     $('#paper').on('click', function(){
         socket.emit('itemSelected', {item: 'paper'});
+        selection($(this), $('#rock'), $('#scissors'));
     });
 
     $('#scissors').on('click', function(){
         socket.emit('itemSelected', {item: 'scissors'});
+        selection($(this), $('#paper'), $('#rock'));
     });
 
 
@@ -64,6 +68,13 @@ $(document).ready(function(){
 
 });
 
+// Adds selected to the first argument, removes it from the others
+function selection(item1, item2, item3){
+    item1.addClass('selected');
+    item2.removeClass('selected');
+    item3.removeClass('selected');
+}
+
 function handleGameAttempt(socket){
     socket.on('foundGame', function (names){
         console.log("Server found a game with: " + names.name.vs);
@@ -72,7 +83,50 @@ function handleGameAttempt(socket){
         $('#actionRow').removeClass('hidden');
         $('#vsRow').removeClass('hidden');
 
+
         $('#nameOfOpponent').text(names.name.vs);
         $('#nameOfYou').text(names.name.you);
+    });
+}
+
+function handleWinLoss(socket){
+    socket.on('result', function (status){
+        var outputStatus = $('#winLoseResults');
+
+        $('#statusRow').removeClass('hidden');
+
+        if(status.status == 'win'){
+            outputStatus.text('Congratulations, you won!');
+        }
+
+        if(status.status == 'lose') {
+            outputStatus.text('You\' just lost!');
+        }
+
+        if(status.status == 'tie') {
+            outputStatus.text('You tied, try again?');
+        }
+
+    });
+}
+
+function handleFinish(socket){
+    socket.on('finish', function (obj){
+        console.log("Finished, cleaned up!");
+
+        // Cleans up rows
+        $('#actionRow').addClass('hidden');
+        $('#statusRow').addClass('hidden');
+        $('#vsRow').addClass('hidden');
+
+        // Allows the option to be put back in the que
+        $('#findGame').removeClass('hidden');
+
+        // Cleans up selected items
+        $('#paper').removeClass('selected');
+        $('#rock').removeClass('selected');
+        $('#scissors').removeClass('selected');
+
+        $('#findGameButton').text("Click to find a game!");
     });
 }
